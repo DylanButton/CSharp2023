917b26f6a36c792b78292ca2e0563b1cda9637dd
DylanButton <dylanbutton1@gmail.com>
2023-03-18 11:23:01 +0000

    initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a second line of code :D");
+            Console.WriteLine("Over And Out");
+            Console.WriteLine(3*3);
+        }
+    }
+}
